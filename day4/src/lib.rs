79f2d2cb2e42ac08d2671de8bdba9b3930f27716
Varshard <varshard0@gmail.com>
2023-12-05 00:31:40 +0700

    day4 part2

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -1,6 +1,6 @@
 mod input;
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -26,7 +26,7 @@ fn read_line(line: &str) -> (HashSet<u32>, Vec<u32>) {
         }
         have.push(n.parse::<u32>().unwrap());
     }
-    
+
     (win, have)
 }
 
@@ -45,8 +45,90 @@ fn sum_win(line: &str) -> u32 {
     })
 }
 
+fn count_win(line: &str) -> u32 {
+    let (win, have) = read_line(line);
+
+    have.iter().fold(0, |sum, x| {
+        match win.get(x) {
+            None => {
+                sum + 0
+            }
+            Some(_) => {
+                sum + 1
+            }
+        }
+    })
+}
+
 pub fn part1(lines: &str) -> u32 {
     lines.split('\n').fold(0, |sum, line | sum + sum_win(line))
+}
+
+pub fn part2(input: &str) -> u32 {
+    let mut cards = HashMap::new();
+    let mut count = 0;
+    for (i, line) in input.split('\n').enumerate() {
+        cards.insert((i+ 1) as u32, count_win(line));
+    }
+
+
+    for (k, _) in input.split('\n').enumerate() {
+        count += process_copy(&((k+1) as u32), &cards);
+    }
+
+    count + cards.len() as u32
+}
+
+fn process_copy(id: &u32, cards: &HashMap<u32, u32>) -> u32 {
+    let mut count = 0;
+    match cards.get(&id) {
+        None => {}
+        Some(v) => {
+            count += v;
+            for i in *id..*id+v+1 {
+                if cards.contains_key(&(i + 1)) {
+                    count += process_copy(&(i + 1), cards);
+                }
+            }
+        }
+    }
+    count
+}
+
+pub fn part2v2(input: &str) -> u32 {
+    let mut cards = HashMap::new();
+    let mut mem = HashMap::new();
+    for (i, line) in input.split('\n').enumerate() {
+        cards.insert((i+ 1) as u32, count_win(line));
+        mem.insert((i+1) as u32, 1);
+    }
+
+    process_copy2(&cards, &mut mem);
+
+    let mut count = 0;
+    for i in 1..cards.len()+1 {
+        count += mem.get(&(i as u32)).unwrap();
+    }
+    count
+}
+
+fn process_copy2(cards: &HashMap<u32, u32>, mut mem: &mut HashMap<u32, u32>) {
+    for i in (1..cards.len()).rev() {
+        let key = &((i) as u32);
+        let win = cards.get(key).unwrap();
+        let mut count = 0;
+
+        for j in i+1..(*win as usize + i + 1) {
+            match mem.get(&(j as u32)) {
+                None => {}
+                Some(c) => {
+                    count += c;
+                    // mem.insert((i - 1) as u32,  c + mem.get(key).unwrap());
+                }
+            }
+        }
+        mem.insert(i as u32, mem.get(key).unwrap() + count);
+    }
 }
 
 #[cfg(test)]
@@ -81,5 +163,100 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"))
     #[test]
     fn test_part1() {
         assert_eq!(18519, part1(input::INPUT));
+    }
+
+    #[test]
+    fn test_process_copy_sample1() {
+        let mut map: HashMap<u32, u32> = HashMap::new();
+        map.insert(1, 2);
+        map.insert(2, 1);
+        map.insert(3, 0);
+        assert_eq!(3, process_copy(&1, &map));
+    }
+
+    #[test]
+    fn test_process_copy_sample2() {
+        let mut map: HashMap<u32, u32> = HashMap::new();
+        map.insert(1, 2);
+        map.insert(2, 2);
+        map.insert(3, 1);
+        map.insert(4, 0);
+        assert_eq!(6, process_copy(&1, &map));
+    }
+
+    #[test]
+    fn test_process_copy2_sample1() {
+        let mut map: HashMap<u32, u32> = HashMap::new();
+        map.insert(1, 2);
+        map.insert(2, 1);
+        map.insert(3, 0);
+
+        let mut mem: HashMap<u32,u32> = HashMap::new();
+        mem.insert(1, 1);
+        mem.insert(2, 1);
+        mem.insert(3, 1);
+
+        process_copy2(&map, &mut mem);
+        assert_eq!(&4, mem.get(&1).unwrap());
+    }
+
+    #[test]
+    fn test_process_copy2_sample2() {
+        let mut map: HashMap<u32, u32> = HashMap::new();
+        map.insert(1, 2);
+        map.insert(2, 2);
+        map.insert(3, 1);
+        map.insert(4, 0);
+
+        let mut mem: HashMap<u32,u32> = HashMap::new();
+        mem.insert(1, 1);
+        mem.insert(2, 1);
+        mem.insert(3, 1);
+        mem.insert(4, 1);
+
+        process_copy2(&map, &mut mem);
+        assert_eq!(&7, mem.get(&1).unwrap());
+    }
+
+    #[test]
+    fn test_process_copy2_sample3() {
+        let mut map: HashMap<u32, u32> = HashMap::new();
+        map.insert(1, 4);
+        map.insert(2, 2);
+        map.insert(3, 2);
+        map.insert(4, 1);
+        map.insert(5, 0);
+        map.insert(6, 0);
+
+        let mut mem: HashMap<u32,u32> = HashMap::new();
+        mem.insert(1, 1);
+        mem.insert(2, 1);
+        mem.insert(3, 1);
+        mem.insert(4, 1);
+        mem.insert(5, 1);
+        mem.insert(6, 1);
+
+        process_copy2(&map, &mut mem);
+        assert_eq!(&15, mem.get(&1).unwrap());
+    }
+
+    #[test]
+    fn test_count_win() {
+        assert_eq!(0, count_win("Card 198: 87  3 64 10 88 45 16 40 23 60 | 63 77 36 52 47 76 84 96 19 13 73 39 26 93 21 22  7 15 95 30 33 89 28 20 50"));
+    }
+
+    #[test]
+    fn test_part2_sample() {
+        assert_eq!(30, part2v2("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"));
+    }
+
+    #[test]
+    fn part2_input() {
+        print!("{}", part2v2(input::INPUT));
     }
 }
